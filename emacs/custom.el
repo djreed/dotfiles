af6b8394795b87a69b42f65a2f222b546de791d2
David James Reed <dreed@alignable.com>
2018-01-19 14:39:22 -0500

    Enhanced ruby mode, package changes

---
+++ emacs/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (smartparens robe smartparens-ruby smartparens-config smartperens-ruby smartperens-config enh-ruby-mode auto-complete zoom-frm yaml-mode which-key use-package undo-tree terraform-mode smooth-scrolling smex smart-tabs-mode rjsx-mode ripgrep restclient rainbow-delimiters racer org-plus-contrib nlinum neotree multiple-cursors mips-mode markdown-mode magit ledger-mode ido-vertical-mode ido-hacks ido-completing-read+ haml-mode flycheck-rust flx-ido exec-path-from-shell elixir-mode diminish diff-hl counsel company-go company-cmake cmake-mode cmake-ide all-the-icons-dired ace-window)))
+ '(terraform-indent-level 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
