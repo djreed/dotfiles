8f4b5f95d321b11e8cf6a123cc631d0cd6d27440
David James Reed <dreed@alignable.com>
2018-02-15 14:58:37 -0500

    No hideshow

---
+++ emacs/custom.el
@@ -0,0 +1,15 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (zoom-frm yasnippet-bundle yaml-mode which-key web-mode web use-package undo-tree terraform-mode tabbar sr-speedbar smooth-scrolling smex smartparens smart-tabs-mode rspec-mode robe rjsx-mode ripgrep restclient rainbow-delimiters racer projectile-rails popwin org-plus-contrib nlinum neotree nav multiple-cursors mips-mode markdown-mode magit ledger-mode ido-vertical-mode ido-hacks ido-completing-read+ haml-mode font-lock+ flycheck-rust flx-ido find-file-in-repository find-file-in-project exec-path-from-shell enh-ruby-mode elixir-mode direx diminish diff-hl counsel-projectile company-go company-cmake cmake-mode cmake-ide auto-complete all-the-icons-dired ace-window)))
+ '(terraform-indent-level 2))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
