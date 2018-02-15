7481977921edb4d0bcacc9c12f8b0e71a76bf4cf
David James Reed <dreed@alignable.com>
2018-02-15 15:35:07 -0500

    Install atom, create and install from package list

--- .atom/package-list.sh
+++
@@ -1,4 +0,0 @@
-apm list --installed --bare > package.list
-git reset
-git add package.list
-git commit -S -m "Update Package List"
--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ ln -sfn ~/dotfiles/emacs/ ~/.emacs.d
 ln -sfn ~/dotfiles/.zshrc ~/.zshrc
 ln -sfn ~/dotfiles/.hyper.js ~/.hyper.js
 ln -sfn ~/dotfiles/fish/ ~/.config/fish
+
+ln -sfn ~/dotfiles/.atom/ ~/.atom
+sh ~/dotfiles/.atom/install-packages.sh
