9baea15a42fc2d575a04427c72c5f9b9550971c4
David J Reed <david.reed@klaviyo.com>
2024-04-02 15:56:41 -0400

    Stop cloning atom packages

--- .pulsar/install-packages.sh
+++ .pulsar/install-packages.sh
@@ -1,2 +1,3 @@
-#/bin/sh
+#!/bin/sh
+
 ppm install --packages-file packages.list
--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 ln -sfn ~/dotfiles/emacs/ ~/
 ln -sfn ~/dotfiles/.zshrc ~/
 ln -sfn ~/dotfiles/.p10k.zsh ~/
-ln -sfn ~/dotfiles/.atom/ ~/
+# ln -sfn ~/dotfiles/.atom/ ~/ ### RIP
 ln -sfn ~/dotfiles/.pulsar/ ~/
 
-cd .atom && sh install-packages.sh
+# cd .atom && sh install-packages.sh ### RIP
+cd .pulsar && sh install-packages.sh
