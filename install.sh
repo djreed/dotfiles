9367699ef91a1b7e789c5108736cb6d3a64e00b5
David J Reed <david.reed@klaviyo.com>
2024-04-02 15:57:09 -0400

    Actually clone atom but don't install anything

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 ln -sfn ~/dotfiles/emacs/ ~/
 ln -sfn ~/dotfiles/.zshrc ~/
 ln -sfn ~/dotfiles/.p10k.zsh ~/
-# ln -sfn ~/dotfiles/.atom/ ~/ ### RIP
+ln -sfn ~/dotfiles/.atom/ ~/ ### RIP
 ln -sfn ~/dotfiles/.pulsar/ ~/
 
 # cd .atom && sh install-packages.sh ### RIP
